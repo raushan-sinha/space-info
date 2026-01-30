137633b74965973e273a3009070de2f2f5fe6959
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-30 14:23:25 +0530

    Installed all files for setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <h1 className='text-red-500'>Space Info</h1>
+    )
+}
+
+export default App
+// https://api.nasa.gov/planetary/apod?api_key=Ita5N8BJFS82UszAH0Ja7XHcUZpqAt8JcRRvcpbP
